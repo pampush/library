56cb6b0781d46d760b22b57f72e04e8b69a9d5be
pampush <32041268+pampush@users.noreply.github.com>
2020-08-11 01:17:57 +0300

    add sort by read status, event on changing item property

--- script.js
+++ script.js
@@ -1,38 +1,62 @@
 function Library() {
   this.myLibrary = new Map();
+  this.sortLib = new Map(); // bad decision
 }
 
 Library.prototype.addBookToLibrary = function(book) {
   if(!book)
     return;
-  this.myLibrary.set(Math.floor(Math.random()*Date.now()), book);
+  const key = Date.now();
+  this.myLibrary.set(key, book);
+  this.sortLib.set(key, book);
 }
 
 Library.prototype.deleteBook = function(index) {
-  if(typeof(index) != 'number')
+   if(typeof(index) != 'number')
     index = +index;
   this.myLibrary.delete(index);
+  this.sortLib.delete(index);
 }
 
-Library.prototype.render = function(parent, library) {
+Library.prototype.sortBooks = function(callback) {
+  //for big amount of items we need to pagination and slicing map/array;
+  this.sortLib = new Map([...this.myLibrary.entries()].sort((a, b) => b[1].hasRead - a[1].hasRead)) // true first
+    //return (a[1].hasRead === b[1].hasRead)? 0: a[1].hasRead? -1: 1;
+}
+
+Library.prototype.changeBook = function(bookNode) {
+  let book = library.myLibrary.get(+bookNode.dataset.index);
+  bookNode.lastChild.classList.remove(`main-item__readstatus-${book.hasRead}`);
+  book.hasRead = !book.hasRead;
+  bookNode.lastChild.classList.add(`main-item__readstatus-${book.hasRead}`)
+}
+
+Library.prototype.render = function(parent) {
+  if(!parent)
+    return;
+  
   while(parent.hasChildNodes()) 
     parent.removeChild(parent.lastChild);
 
-  if(!(library && parent))
-    return;
+  let library = this.myLibrary;
+  if(parent.dataset.sorted == 'true')
+    library = this.sortLib;
 
   for(let [key, book] of library) { 
     const div = document.createElement('div'),
           divDel = document.createElement('div'),
+          divStatus = document.createElement('div'),
           h1 = document.createElement('h1'), 
           h2 = document.createElement('h2');
-    
+
     h1.append(book.title);
     h2.append(book.author);
     divDel.classList.add('main-item__delete');
+    divStatus.classList.add('main-item__readstatus');
+    divStatus.classList.add(`main-item__readstatus-${book.hasRead}`);
     div.classList.add('main-item');
     div.setAttribute('data-index', key);
-    div.append(divDel, h1, h2);
+    div.append(divDel, h1, h2, divStatus);
     parent.append(div);
   }
 }
@@ -42,11 +66,10 @@ function Book(title, author, numberOfPages, hasRead, ...rest) {
   this.author = author;
   this.numberOfPages = numberOfPages;
   this.hasRead = hasRead;
-  //this.id = Book.prototype.generateID();
 }
 
-Book.prototype.generateID = function() {
-  return Math.floor(Math.random()*1000);
+Book.prototype.changeReadStatus = function() {
+
 }
 
 function getFormData(form) {
@@ -55,13 +78,17 @@ function getFormData(form) {
   for(let item of form.elements) {
     if(item.type == 'text')
       book[item.name] = item.value; 
+    /* change radio with tumbler. that is for learning */
     if(item.type == 'radio' && !checkedFlag) {
       if(item.checked) { 
         checkedFlag = true;
-        book[item.name] = item.value;
+        if(item.value == 'true')
+          book[item.name] = true;
+        else
+          book[item.name] = false;
       }
       else
-        book[item.name] = item.value;
+        book[item.name] = false;
     }
   }
   return book;
@@ -73,12 +100,24 @@ const createBookButton = document.querySelector('button[value="createBook"]'),
       modalBox = document.querySelector('.modal'),
       modalBoxForm = document.querySelector('.modal-form'),
       //bookItem = document.querySelector('.main-item'),
-      library = new Library();
+      library = new Library(); // init with localStorage
 
 createBookButton.addEventListener('click',  () => { 
   modalBox.style.display = 'block'; 
   modalBox.querySelector('input').focus(); // first input 
+});
 
+sortBookButton.addEventListener('click', (e) => { 
+  if(booksContainer.dataset.sorted == 'true') {
+    booksContainer.setAttribute('data-sorted', 'false');
+    e.target.style.backgroundColor = 'red';
+  }
+  else { 
+    e.target.style.backgroundColor = 'green';
+    booksContainer.setAttribute('data-sorted', 'true');
+    library.sortBooks();
+  } 
+  library.render(booksContainer);
 });
 
 modalBoxForm.addEventListener('submit', (e) => {
@@ -86,16 +125,21 @@ modalBoxForm.addEventListener('submit', (e) => {
   const data = getFormData(e.target); 
   const book = new Book(...Object.values(data)); 
   library.addBookToLibrary(book); 
-  library.render(booksContainer, library.myLibrary);
+  library.render(booksContainer);
   modalBox.style.display = 'none';
   modalBoxForm.reset();
-  /* check out formData object */
 });
 
 booksContainer.addEventListener('click', (e) => {
   if(e.target.classList == 'main-item__delete') {
-    library.deleteBook(e.target.parentNode.dataset.index);
-    library.render(booksContainer, library.myLibrary);
+    library.deleteBook(e.target.parentNode.dataset.index); // or remove a single item instead?
+    library.render(booksContainer);
+  }else
+  if(e.target.classList[0] == 'main-item__readstatus') {
+    library.changeBook(e.target.parentNode);
+  }else
+  if(e.target.classList == 'main-item') {
+    console.log('ok');
   }
 })
 
